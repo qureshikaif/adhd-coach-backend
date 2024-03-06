4f34e8431f46a47d291ccc80ed96e21f3e5c97ea
qureshikaif <qureshik488@gmail.com>
2024-03-06 21:54:03 +0500

    feat: added websocket connection

--- server.ts
+++ server.ts
@@ -1,7 +1,16 @@
 import express from "express";
 import cors from "cors";
+import { Server } from "socket.io";
+import http from "http";
 
 const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
 
 app.use(cors());
 app.use(express.json());
@@ -10,6 +19,19 @@ app.get("/", (req: express.Request, res: express.Response) => {
   res.json({ message: "Hello World" });
 });
 
-app.listen(8080, () => {
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+    io.emit("chat message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+server.listen(8080, () => {
   console.log("Server is running on port 8080");
 });
