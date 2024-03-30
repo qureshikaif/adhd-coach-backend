88cda1162b47d25d73697b4116aadfc22bd1048a
qureshikaif <qureshik488@gmail.com>
2024-03-30 15:35:33 +0500

    feat: updated database schema

--- db.ts
+++ db.ts
@@ -31,8 +31,28 @@ const createTable = async (tableName: string, columns: string) => {
 };
 
 createTable(
-  "nft",
-  "id SERIAL PRIMARY KEY, current_owner TEXT, primary_owner TEXT, title TEXT, image_name TEXT, image_url TEXT, description TEXT, writer TEXT, genre TEXT, characters TEXT, runtime TEXT, on_sale BOOLEAN, price TEXT, expiry TEXT, royalties TEXT, token_id TEXT, collection_address TEXT, image_ipfs_hash TEXT, nft_ipfs_hash TEXT"
+  "courses",
+  "id SERIAL PRIMARY KEY, title TEXT, description TEXT, price TEXT, image TEXT, instructor TEXT, rating TEXT, students TEXT, duration TEXT, on_sale BOOLEAN, expiry TEXT, royalties TEXT, token_id TEXT, collection_address TEXT, image_ipfs_hash TEXT, nft_ipfs_hash TEXT"
+);
+
+createTable(
+  "students",
+  "id SERIAL PRIMARY KEY, email TEXT, primary_owner TEXT, title TEXT, image_name TEXT, image_url TEXT, description TEXT, writer TEXT, genre TEXT, characters TEXT, runtime TEXT, on_sale BOOLEAN, price TEXT, expiry TEXT, royalties TEXT, token_id TEXT, collection_address TEXT, image_ipfs_hash TEXT, nft_ipfs_hash TEXT"
+);
+
+createTable(
+  "parents",
+  "id SERIAL PRIMARY KEY, username TEXT, password TEXT, email TEXT, wallet_address TEXT, role TEXT"
+);
+
+createTable(
+  "doctors",
+  "id SERIAL PRIMARY KEY, name , seller TEXT, token_id TEXT, price TEXT, transaction_date TEXT"
+);
+
+createTable(
+  "teachers",
+  "id SERIAL PRIMARY KEY, buyer TEXT, seller TEXT, token_id TEXT, price TEXT, transaction_date TEXT"
 );
 
 export { pool };
